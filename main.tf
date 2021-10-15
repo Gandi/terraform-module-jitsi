79575eb9891a8ff6237b7fa475c721e4659b11ff
Guillaume Doré <guillaume.dore@gandi.net>
2021-10-15 10:56:07 +0200

    Ran terraform fmt

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 terraform {
-required_version = ">= 1.0"
+  required_version = ">= 1.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
       version = "~> 1.35.0"
     }
     gandi = {
-      source = "psychopenguin/gandi"
+      source  = "psychopenguin/gandi"
       version = "2.0.0-rc3"
     }
   }
 }
 
 data "template_file" "user_data" {
-  template = "${file("${path.module}/templates/cloud-init.yaml")}"
+  template = file("${path.module}/templates/cloud-init.yaml")
 
   vars = {
-    domain = "${var.dns_subdomain}.${var.dns_zone}"
-    email = "${var.letsencrypt_email}"
+    domain         = "${var.dns_subdomain}.${var.dns_zone}"
+    email          = "${var.letsencrypt_email}"
     use_le_staging = "${var.letsencrypt_staging}"
   }
 }
@@ -27,20 +27,20 @@ resource "openstack_compute_keypair_v2" "jitsi-keypair" {
 }
 
 resource "openstack_compute_instance_v2" "jitsi" {
-  name = "jitsi"
-  key_pair = openstack_compute_keypair_v2.jitsi-keypair.name
-  flavor_name = "V-R2"
+  name            = "jitsi"
+  key_pair        = openstack_compute_keypair_v2.jitsi-keypair.name
+  flavor_name     = "V-R2"
   security_groups = ["default"]
-  power_state = "active"
+  power_state     = "active"
   network {
     name = "public"
   }
 
-  user_data = "${data.template_file.user_data.rendered}"
+  user_data = data.template_file.user_data.rendered
 
   block_device {
     # Ubuntu 20.04
-    uuid = "47edd0a0-23ce-4ce5-9168-36de68990d1b"
+    uuid                  = "47edd0a0-23ce-4ce5-9168-36de68990d1b"
     source_type           = "image"
     volume_size           = 25
     boot_index            = 0
@@ -50,23 +50,23 @@ resource "openstack_compute_instance_v2" "jitsi" {
 }
 
 resource "local_file" "private-key" {
-  content     = openstack_compute_keypair_v2.jitsi-keypair.private_key
-  filename = "jitsi-priv.key"
+  content         = openstack_compute_keypair_v2.jitsi-keypair.private_key
+  filename        = "jitsi-priv.key"
   file_permission = "0600"
 }
 
 resource "gandi_livedns_record" "jitsi_dns_v4" {
-  zone = var.dns_zone
-  name = var.dns_subdomain
-  type = "A"
-  ttl = 1800
+  zone   = var.dns_zone
+  name   = var.dns_subdomain
+  type   = "A"
+  ttl    = 1800
   values = ["${openstack_compute_instance_v2.jitsi.access_ip_v4}"]
 }
 
 resource "gandi_livedns_record" "jitsi_dns_v6" {
-  zone = var.dns_zone
-  name = var.dns_subdomain
-  type = "AAAA"
-  ttl = 1800
+  zone   = var.dns_zone
+  name   = var.dns_subdomain
+  type   = "AAAA"
+  ttl    = 1800
   values = [trim("${openstack_compute_instance_v2.jitsi.access_ip_v6}", "[]")]
 }
